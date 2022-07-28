2b9c2820d6304035d5553624b8319911683aa34f
Aishat Olowoshile <aolowoshile@myriadmobile.com>
2022-07-27 20:43:31 -0400

    fixed redundant conformance build error

---
+++ ExpenseLog+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseLog+CoreDataClass.swift
+//  ExpenseTracker
+//
+//  Created by Aishat Olowoshile on 7/27/22.
+//  Copyright © 2022 Alfian Losari. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ExpenseLog)
+public class ExpenseLog: NSManagedObject {
+
+}
